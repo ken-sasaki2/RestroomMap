8f36605fcd0229b49da6b74df89decfbbc8f5ae0
佐々木　謙 <nkekisasa222@gmail.com>
2022-05-10 02:17:17 +0900

    Initial Commit

---
+++ RestroomMap/RestroomMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestroomMapApp.swift
+//  RestroomMap
+//
+//  Created by sasaki.ken on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct RestroomMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
