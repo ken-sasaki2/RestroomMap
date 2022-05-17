ea2dd63f5d7abc182ea382c5fbee074c19f67e83
佐々木　謙 <nkekisasa222@gmail.com>
2022-05-17 11:38:17 +0900

    update: 位置情報許可のData層書いた

---
+++ RestroomMap/Data/DataStore/LocatePermissionDataStore.swift
@@ -0,0 +1,96 @@
+//
+//  LocatePermissionDataStore.swift
+//  RestroomMap
+//
+//  Created by sasaki.ken on 2022/05/15.
+//
+
+import CoreLocation
+
+protocol LocatePermissionDataStoreInterface {
+    var delegate: LocatePermissionDataStoreDelegate? { get set }
+    func getAuthorizationStatus() -> AuthorizationStatusEntity
+    func requestWhenInUse(_ complication: @escaping (AuthorizationStatusEntity) -> Void)
+    func startUpdatingLocation()
+}
+
+protocol LocatePermissionDataStoreDelegate: AnyObject {
+    func didUpdatedLocation(_ dataStore: LocatePermissionDataStore)
+    func didFailWithError(_ dataStore: LocatePermissionDataStore)
+}
+
+
+final class LocatePermissionDataStore: NSObject, LocatePermissionDataStoreInterface, CLLocationManagerDelegate {
+    private let locationManager: CLLocationManager
+    private var statusEntity: AuthorizationStatusEntity
+    weak var delegate: LocatePermissionDataStoreDelegate?
+    var callback: ((AuthorizationStatusEntity) -> Void)?
+
+    override init() {
+        locationManager = CLLocationManager()
+        statusEntity = .unknown
+        super.init()
+        locationManager.delegate = self
+    }
+
+
+    func getAuthorizationStatus() -> AuthorizationStatusEntity {
+        let status = statusEntity
+        return status
+    }
+
+
+    func requestWhenInUse(_ complication: @escaping (AuthorizationStatusEntity) -> Void) {
+        locationManager.requestWhenInUseAuthorization()
+        callback = complication
+    }
+
+
+    func startUpdatingLocation() {
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = manager.authorizationStatus
+
+        switch status {
+        case .notDetermined:
+            statusEntity = .notDetermined
+        case .restricted:
+            statusEntity = .restricted
+        case .denied:
+            statusEntity = .denied
+        case .authorizedAlways:
+            statusEntity = .authorizedAlways
+        case .authorizedWhenInUse:
+            statusEntity = .authorizedWhenInUse
+        @unknown default:
+            statusEntity = .unknown
+        }
+
+        callback?(statusEntity)
+        callback = nil
+    }
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+
+        let lat = Double(location.coordinate.latitude)
+        let lng = Double(location.coordinate.longitude)
+        print("latitude:", lat, "longitude:", lng)
+
+        delegate?.didUpdatedLocation(self)
+        locationManager.stopUpdatingLocation()
+    }
+
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        delegate?.didFailWithError(self)
+    }
+}
