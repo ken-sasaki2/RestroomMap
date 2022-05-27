d06663a2893cc31db0cc059e9a8924c30038d198
佐々木　謙 <nkekisasa222@gmail.com>
2022-05-27 14:41:09 +0900

    update: レビュー機能書いた

--- RestroomMap/Domain/UseCase/ReviewUseCase.swift
+++ RestroomMap/Domain/UseCase/ReviewUseCase.swift
@@ -5,16 +5,24 @@
 //  Created by sasaki.ken on 2022/05/21.
 //
 
-import Foundation
+import StoreKit
 
 protocol ReviewUseCaseInterface {
-
+    func requestReview()
 }
 
 final class ReviewUseCase: ReviewUseCaseInterface {
     private let presenter: MenuPresenter
 
+    
     init(presenter: MenuPresenter) {
         self.presenter = presenter
+    }
+
+    
+    func requestReview() {
+        if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: scene)
+        }
     }
 }
