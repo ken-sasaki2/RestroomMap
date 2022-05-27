ffc924238dac23af154eef847c23ac1f979ec0b1
佐々木　謙 <nkekisasa222@gmail.com>
2022-05-27 14:44:32 +0900

    add: 定数管理するファイル追加

---
+++ RestroomMap/Application/Const/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  RestroomMap
+//
+//  Created by sasaki.ken on 2022/05/22.
+//
+
+import Foundation
+
+struct Const {
+    static let inquiryUrl = URL(string: "https://docs.google.com/forms/d/11mbsJ-Zy46PeAwnVROIgY8qkBg6lAu0RijAyQw_bQ9c/edit?usp=sharing")
+    static let shareText = "公衆トイレ共有アプリ"
+    static let shareUrl = "https://qiita.com/ken_sasaki2"
+    static let shareImage = "focus"
+}
