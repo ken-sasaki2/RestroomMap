93d8e9cec7845b179e89508a34d629ddda5dd2a3
佐々木　謙 <nkekisasa222@gmail.com>
2022-06-17 15:18:20 +0900

    test: AppDelegateControllerTests書いた

---
+++ RestroomMapTests/Presentation/Controller/AppDelegateControllerTests.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegateControllerTests.swift
+//  RestroomMapTests
+//
+//  Created by sasaki.ken on 2022/06/17.
+//
+
+import XCTest
+
+class AppDelegateControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func test_saveLaunchCount() throws {
+        let useCase = MockUserUseCase()
+        let controller = AppDelegateController(userUseCase: useCase)
+
+        controller.saveLaunchCount()
+        XCTAssertEqual(useCase.isSaveLaunchCountCalled, true)
+    }
+
+
+    func test_getLaunchCount() throws {
+        let useCase = MockUserUseCase()
+        let controller = AppDelegateController(userUseCase: useCase)
+
+        controller.getLaunchCount()
+        XCTAssertEqual(useCase.isGetLaunchCountCalled, true)
+    }
+}
