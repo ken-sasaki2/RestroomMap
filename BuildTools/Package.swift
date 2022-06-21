6369f0dc3c07cf8fcfa890b112c587a64f49e431
佐々木　謙 <nkekisasa222@gmail.com>
2022-06-21 18:17:27 +0900

    Update Package.swift

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -3,9 +3,21 @@ import PackageDescription
 
 let package = Package(
     name: "BuildTools",
-    platforms: [.macOS(.v10_11)],
+    platforms: [.iOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", .exact("0.43.1")),
+        .package(url: "https://github.com/realm/SwiftLint", .upToNextMajor(from: "0.43.1")),
+        // .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.0")),
     ],
-    targets: [.target(name: "BuildTools", path: "")]
+    targets: [
+        .target(
+            name: "BuildTools",
+            path: ""
+        ),
+//        .target(
+//            name: "RestroomMap",
+//            dependencies: [
+//                .product(name: "FirebaseAuth", package: "Firebase"),
+//            ]
+//        )
+    ]
 )
