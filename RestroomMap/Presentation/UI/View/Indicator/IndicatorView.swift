6c2d8760b510956ea17b12a7ab5ccd39ab1ff66d
佐々木　謙 <nkekisasa222@gmail.com>
2022-06-23 07:29:24 +0900

    update: indicatorのUI変えた

--- RestroomMap/Presentation/UI/View/Indicator/IndicatorView.swift
+++ RestroomMap/Presentation/UI/View/Indicator/IndicatorView.swift
@@ -12,17 +12,7 @@ struct IndicatorView: View {
         ZStack {
             Color.black.opacity(0.3)
                 .edgesIgnoringSafeArea(.all)
-            ZStack {
-                Color.black.opacity(0.6)
-                    .frame(width: 150, height: 120)
-                    .cornerRadius(8)
-                VStack {
-                    ActivityIndicatorView()
-                    Text("準備中...")
-                        .foregroundColor(.white)
-                        .font(.system(size: 18, weight: .regular, design: .default))
-                }
-            }
+            ActivityIndicatorView()
         }
     }
 }
