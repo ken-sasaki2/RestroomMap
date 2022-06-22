b1b7fc0731394e3eeb5f11342dd7c77fa0f8ff75
佐々木　謙 <nkekisasa222@gmail.com>
2022-06-22 23:11:14 +0900

    update: Firestoreとの通信処理を書いた（保存）

--- RestroomMap/Data/Network/LocationRequestToFirestore.swift
+++ RestroomMap/Data/Network/LocationRequestToFirestore.swift
@@ -5,11 +5,37 @@
 //  Created by sasaki.ken on 2022/06/21.
 //
 
-import Foundation
+import Firebase
 
 final class LocationRequestToFirestore {
+    private let reference = Firestore.firestore().collection("locations")
 
-    func save(_ model: LocationAddInputModel) {
-        // send model...
+
+    func save(_ model: LocationAddInputModel) async throws {
+        do {
+            try await reference.document().setData([
+                "lat": model.lat,
+                "lng": model.lng,
+                "name": model.name,
+                "isOpen24Hour": model.isOpen24Hour,
+                "openDate": model.openDate,
+                "closeDate": model.closeDate,
+                "holiday": model.holiday as Any,
+                "isWesternStyle": model.isWesternStyle,
+                "isJapaneseStyle": model.isJapaneseStyle,
+                "isPublic": model.isPublic,
+                "isByGender": model.isByGender,
+                "isWashlet": model.isWashlet,
+                "isMultipurpose": model.isMultipurpose,
+                "isWheelchair": model.isWheelchair,
+                "isDiaper": model.isDiaper,
+                "isBed": model.isBed,
+                "isPowderRoom": model.isPowderRoom,
+                "isParking": model.isParking,
+                "memo": model.memo as Any
+            ])
+        } catch {
+            throw error
+        }
     }
 }
